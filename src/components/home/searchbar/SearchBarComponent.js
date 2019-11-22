76920d3015e2b67b6e13c9bc509e892a1c0030e4
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-21 23:42:21 -0500

    Added a submit handler aswell as connected component to store

--- src/components/home/searchbar/SearchBarComponent.js
+++ src/components/home/searchbar/SearchBarComponent.js
@@ -4,16 +4,41 @@ import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import InputGroup from 'react-bootstrap/InputGroup'
+import { connect } from 'react-redux'
+import { fetchMovies } from '../../../redux/ActionCreators'
 
-export default class SearchBar extends PureComponent {
+const mapDispatchToProps = dispatch => ({
+    fetchMovies: value => dispatch(fetchMovies(value))
+})
+
+class SearchBar extends PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            searchValue: ''
+        }
+
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+
+        this.props.fetchMovies(this.state.searchValue)
+
+        return this.setState({ searchValue: '' })
+    }
+
     render() {
         return (
             <Row className="mt-4">
                 <Col xs="12" md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }}>
-                    <Form>
+                    <Form onSubmit={this.handleSubmit}>
                         <Form.Row>
                             <InputGroup className="mb-3">
-                                <Form.Control type="search" placeholder="Search for a movie here..." />
+                                <Form.Control type="search" placeholder="Search for a movie here..." value={this.state.searchValue} onChange={e => this.setState({ searchValue: e.target.value })} />
                                 <InputGroup.Prepend>
                                     <Button>Search</Button>
                                 </InputGroup.Prepend>
@@ -25,3 +50,5 @@ export default class SearchBar extends PureComponent {
         )
     }
 }
+
+export default connect(null, mapDispatchToProps)(SearchBar)
