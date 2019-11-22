f46b7dd93c3a679e000db2495df107df88aad364
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-21 21:32:29 -0500

    Eddited a bug

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,8 +3,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
-
-export const configureStore = () => {
+export const ConfigureStore = () => {
     // This is for the redux Devtools to work correctly. Remove before building
     const compose = enhancer => {return enhancer},
           composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
