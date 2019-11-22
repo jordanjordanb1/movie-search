ce1c29847cedfc8c7695a6557b03206356857048
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-21 21:32:39 -0500

    Added home component

---
+++ src/components/home/HomeComponent.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+import './HomeComponent.css'
+
+export default class Home extends PureComponent {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
