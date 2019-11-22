5aed2558bf161da005b8fd79a461a2d408f489cd
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-21 21:37:47 -0500

    Added header component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,13 @@
 import React, { PureComponent } from 'react'
 import './MainComponent.css'
 import Home from './home/HomeComponent'
+import Header from './shared/header/HeaderComponent'
 
 export default class Main extends PureComponent {
     render() {
         return (
             <>
+                <Header />
                 <Home />
             </>
         )
---
+++ src/components/shared/header/HeaderComponent.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+import './HeaderComponent.css'
+
+export default class Header extends PureComponent {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
