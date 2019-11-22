7e0dab6ebb358d0b69a6fea86e96ee807a79ae97
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-21 23:42:01 -0500

    Added a insert movies action

--- src/redux/movies.js
+++ src/redux/movies.js
@@ -2,6 +2,8 @@ import * as ActionTypes from './ActionTypes'
 
 export const movies = (state = { moviesList: null }, action) => {
     switch(action.type) {
+        case ActionTypes.INSERT_MOVIES:
+            return { ...state, moviesList: action.payload }
         default: 
             return state
     }
