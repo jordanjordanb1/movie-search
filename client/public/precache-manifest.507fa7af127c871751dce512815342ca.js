ef7a80517ee18cd2dceb490b3314ab7d05437296
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-23 18:18:28 -0500

    Changed api req to https from http

---
+++ client/public/precache-manifest.507fa7af127c871751dce512815342ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e12df040ea8cdf8c9708e68b40b3d2",
+    "url": "../public/index.html"
+  },
+  {
+    "revision": "d620e59f144cbc1f733a",
+    "url": "../public/static/css/main.33e3bb57.chunk.css"
+  },
+  {
+    "revision": "4aaccf76436b174519a1",
+    "url": "../public/static/js/2.52ae0b25.chunk.js"
+  },
+  {
+    "revision": "d620e59f144cbc1f733a",
+    "url": "../public/static/js/main.ad36e938.chunk.js"
+  },
+  {
+    "revision": "a0b8d429b0da76a97a55",
+    "url": "../public/static/js/runtime-main.301a8d2b.js"
+  },
+  {
+    "revision": "6c768850e3928a042d54eccc11b43f19",
+    "url": "../public/static/media/not-found.6c768850.png"
+  }
+]);
--- client/src/redux/ActionCreators.js
+++ client/src/redux/ActionCreators.js
@@ -7,7 +7,7 @@ export const fetchMovies = value => dispatch => {
 
     // Checks to see if a value was set. Not really needed but...
     if (value) {
-        axios.get(`http://www.omdbapi.com/?apikey=${omdb_api_key}&s=${value}`) // Does a get request to the API with the required params
+        axios.get(`https://www.omdbapi.com/?apikey=${omdb_api_key}&s=${value}`) // Does a get request to the API with the required params
              .then(res => {
                  if (res.status === 200) { // Checks to see if a response came back
                     if (res.data) { // Checks to see if any movies were found
