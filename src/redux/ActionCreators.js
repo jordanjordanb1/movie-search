7946d5f0713812be09e4ffaf25a600a5e0a94df3
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-22 23:14:21 -0500

    Added comments

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,24 +1,26 @@
 import axios from 'axios'
 import * as ActionTypes from './ActionTypes'
 
+// Fetches movies from omdb api
 export const fetchMovies = value => dispatch => {
     const omdb_api_key = 'ab8a3aca'
 
+    // Checks to see if a value was set. Not really needed but...
     if (value) {
-        axios.get(`http://www.omdbapi.com/?apikey=${omdb_api_key}&s=${value}`)
+        axios.get(`http://www.omdbapi.com/?apikey=${omdb_api_key}&s=${value}`) // Does a get request to the API with the required params
              .then(res => {
-                 if (res.status === 200) {
-                    if (res.data) {
-                        dispatch({
+                 if (res.status === 200) { // Checks to see if a response came back
+                    if (res.data) { // Checks to see if any movies were found
+                        dispatch({ // If found, insert movies into redux store
                             type: ActionTypes.INSERT_MOVIES,
-                            payload: res.data.Search
+                            payload: res.data.Search // Found movies
                         })
-                        dispatch({
+                        dispatch({ // Also tick the api lookup amount up 1
                             type: ActionTypes.API_LOOKUP
                         })
                     }
                  } else {
-                     console.error('An unknown error occured')
+                     console.error('An unknown error occured') // If the server responded with any other than 200, then maybe server is down?
                  }
              })
     }
