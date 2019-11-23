8a5a5dc1a09757caca4a7aea3d3e6e9078c1dcd6
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-23 17:42:35 -0500

    Addec cors and removed cookieparser

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express'),
       path = require('path'),
-      cookieParser = require('cookie-parser'),
       logger = require('morgan'),
-      bodyParser = require('body-parser')
+      bodyParser = require('body-parser'),
+      cors = require('cors')
 
 const indexRouter = require('./routes/index')
 
@@ -10,10 +10,10 @@ const app = express();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors({ origin: true }))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
