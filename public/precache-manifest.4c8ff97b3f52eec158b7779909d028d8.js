84d89933c8f6641fe21f91efca772471dffe23cc
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-23 18:07:33 -0500

    new build

---
+++ public/precache-manifest.4c8ff97b3f52eec158b7779909d028d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d640a93e8422257c3e87bb126d93bbfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb21423151cade8a1c5b",
+    "url": "/static/css/main.33e3bb57.chunk.css"
+  },
+  {
+    "revision": "50eb7352d79181356553",
+    "url": "/static/js/2.e2718435.chunk.js"
+  },
+  {
+    "revision": "fb21423151cade8a1c5b",
+    "url": "/static/js/main.2cd62ef0.chunk.js"
+  },
+  {
+    "revision": "c5aa7731d4f5bd87300f",
+    "url": "/static/js/runtime-main.1e01c251.js"
+  },
+  {
+    "revision": "6c768850e3928a042d54eccc11b43f19",
+    "url": "/static/media/not-found.6c768850.png"
+  }
+]);
