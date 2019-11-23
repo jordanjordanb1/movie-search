9554eace44445a90b7cb1eeef50c0b301c4af1c3
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-23 18:00:07 -0500

    Added some prod checks and removed body-parser as it's not needed

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express'),
       path = require('path'),
       logger = require('morgan'),
-      bodyParser = require('body-parser'),
       cors = require('cors')
 
 const indexRouter = require('./routes/index')
 
 const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({limit: '12mb', extended: true }))
+
 app.use(cors({ origin: true }))
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
+// Enables logger in development mode
+if (process.env.ENV !== 'PROD')
+    app.use(logger('dev'));
+
+app.use(express.json({limit: '12mb'}));
+app.use(express.urlencoded({ limit: '12mb', extended: true }));
+
+// Static path
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* Routes  */
 app.use('/', indexRouter);
 
 module.exports = app;
