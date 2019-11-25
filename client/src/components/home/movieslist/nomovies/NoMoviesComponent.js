77e13ebc82f233329770a6d6d1c2ccd6e5665d9d
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-25 18:04:43 -0500

    Turned NoMovies dumb

--- client/src/components/home/movieslist/nomovies/NoMoviesComponent.js
+++ client/src/components/home/movieslist/nomovies/NoMoviesComponent.js
@@ -1,12 +1,10 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import './NoMoviesComponent.css'
 
-export default class NoMovies extends PureComponent {
-    render() {
-        return (
-            <div className="no-movie">
-                <h1>Please search for a movie first</h1>
-            </div>
-        )
-    }
+export default function NoMovies() {
+    return (
+        <div className="no-movie">
+            <h1>Please search for a movie first</h1>
+        </div>
+    )
 }
